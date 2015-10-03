b6dd77b6a80e989aeb3db57a122d953c803842ff
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-03 15:29:53 +0100

    Added todos

--- public/javascripts/views/createEventView.js
+++ public/javascripts/views/createEventView.js
@@ -4,17 +4,23 @@ define(["backbone"],
             //el: $("#usertable"), // the element tag of this view
             //className: "dashboard",
 
-            // some init
+            // some init here, for example detect view changes
             initialize: function () {
             },
 
-            // some render
+            // render function here, usually returns the view itself
             render: function(){
 
             }
 
-            //TODO: add some functions which select tags and parses info user enters
-            //eg. $(.date).val() ...
+            /*
+            TODO: try using collection's toJSON method to get all the "events" objects,
+            then try attaching those events to the dropdown box using jquery function
+            after that you can use underscore's filter or find function to get properties
+            of just the events that are selected by the user
+            */
+
+
 
 
 
