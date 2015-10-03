b7ecc955b6621d7582399cc07f9822b040afb7f3
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-03 16:45:27 +0100

    Added generatedEventListView

---
+++ public/javascripts/views/generatedEventListView.js
@@ -0,0 +1,19 @@
+define(["backbone"],
+    function (Backbone) {
+        var generatedEventListView = Backbone.View.extend({
+            //el: $("#usertable"), // the element tag of this view
+            //className: "dashboard",
+
+            // some init here, for example detect view changes
+            initialize: function () {
+            },
+
+            // render function here, returns the view itself
+            render: function(){
+
+
+                return this;
+            }
+        });
+        return generatedEventListView;
+    });
