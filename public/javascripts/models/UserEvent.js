8550b7b9dfd14f9bfc7fb8e5cc9c28bd1b17c9a4
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-03 14:53:25 +0100

    Added UserEvent model and createEventView view

---
+++ public/javascripts/models/UserEvent.js
@@ -0,0 +1,5 @@
+define(["backbone"], function(Backbone){
+    var UserEvent = Backbone.Model.extend({
+    });
+    return UserEvent;
+});
---
+++ public/javascripts/views/createEventView.js
@@ -0,0 +1,25 @@
+define(["backbone"],
+    function (Backbone) {
+        var createEventView = Backbone.View.extend({
+            //el: $("#usertable"), // the element tag of this view
+            //className: "dashboard",
+
+            // some init
+            initialize: function () {
+            },
+
+            // some render
+            render: function(){
+
+            }
+
+            //TODO: add some functions which select tags and parses info user enters
+            //eg. $(.date).val() ...
+
+
+
+
+
+        });
+        return createEventView;
+    });
