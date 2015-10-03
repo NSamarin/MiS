ad4c57e1f15b64ba7d180bcfee6e272c57036705
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-03 15:10:06 +0100

    Added userEventsCollection + Updated API

--- routes/events.js
+++ routes/events.js
@@ -4,12 +4,18 @@ var router = express.Router();
 var sample = [
   {
     "name": "Ghost Tour 1",
+    "dates": [{
+      "Monday": ["15:30", "18:30"],
+      "Wednesday": ["11:30", "19:45"],
+      "Friday": ["22:00"]
+    }],
     "discount": 0.1,
     "number": 20,
     "expires": 3
+
   },
   {
-    "name": "Ghost Tour 1",
+    "name": "Ghost Tour 2",
     "discount": 0.2,
     "number": 40,
     "expires": 5
