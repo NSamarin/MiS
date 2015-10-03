246206a7e0d3ab6dab992b80e77a64eadd1b13af
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-03 15:55:57 +0100

    Created new route for adding new event

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,11 +1,39 @@
 //Backbone.Router implemented here
 
-define(["backbone", "collections/userEventCollection"],
-    function (Backbone, UserEventCollection) {
+define(["backbone", "collections/userEventCollection", "views/createEventView"],
+    function (Backbone, UserEventCollection, CreateEventView) {
 
         var Router = Backbone.Router.extend({
             initialize: function () {
-                console.log(UserEventCollection);
+                var self = this;
+                Backbone.Events.on("router:navigate", function (url) {
+                    self.navigate(url, {trigger: true});
+                });
+            },
+            routes: {
+                "": "index",
+                "create": "addNewEvent"
+            },
+            _renderView: function (view) {
+                //detaches element, resetting events
+                var renderedView = view.render();
+
+                //renderedView.$el.detach();
+                //$("#app").empty().append(renderedView.el);
+            },
+            index: function () {
+            },
+            addNewEvent: function () {
+                var eventCollection = new UserEventCollection();
+                var self = this;
+                eventCollection.fetch({
+                    success: function (events) {
+                        var createEventView = new CreateEventView({
+                            collection: events
+                        });
+                        self._renderView(createEventView);
+                    }
+                });
             }
         });
         return Router;
