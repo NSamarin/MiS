7346a53578b5685a068755f01d9907aaf33b1124
comRamona <com.ramona@yahoo.com>
2015-10-03 22:50:52 +0100

    fix

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('abcd');  
   
 db.serialize(function() {  
-  db.run("CREATE TABLE user (id INT, dt TEXT)");  
+  db.run("CREATE TABLE if not exists user (id INT, dt TEXT)");  
   
   var stmt = db.prepare("INSERT INTO user VALUES (?,?)");  
   for (var i = 0; i < 10; i++) {  
