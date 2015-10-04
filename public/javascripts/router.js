7e12fdb21db7b748219a48905abfa2a595ac6f53
Nikita Samarin <N.Samarin.S@gmail.com>
2015-10-04 17:01:09 +0100

    some changes

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,12 +1,13 @@
 //Backbone.Router implemented here
 
-define(["backbone", "collections/venueCollection", "collections/eventCollection", "helpers", "models/event"],
-    function (Backbone, VenueCollection, EventCollection, Helpers, Event) {
+define(["backbone", "collections/venueCollection", "collections/eventCollection", "helpers", "models/event", "handlebars"],
+    function (Backbone, VenueCollection, EventCollection, Helpers, Event, Handlebars) {
 
         var Router = Backbone.Router.extend({
             routes: {
                 "": "index",
-                "events/:id": "detailedView"
+                "events/:id": "detailedView",
+                "events": "listView"
             },
 
             initialize: function () {
@@ -69,20 +70,22 @@ define(["backbone", "collections/venueCollection", "collections/eventCollection"
             detailedView: function (id) {
                 console.log("ping from list view");
                 console.log(id);
+                var self = this;
                 var eventCollection = new EventCollection();
                 eventCollection.fetch({
                     success: function(data){
+                        console.log(data);
                         var event = data.find(function(ev){
                             return ev.get("id") == id;
                         });
-                        console.log(event);
+                        if (event) self._populateDetailedView(event);
+                        else {
+                            Backbone.Events.trigger("router:navigate", "/events");
+                            location.reload();
+                        }
+
                     }
                 });
-
-                //TODO: create collection instead and get the model with required ID!!
-
-
-
                 //var event = new Event();
                 //event.fetch({
                 //    success: function(data){
@@ -91,10 +94,29 @@ define(["backbone", "collections/venueCollection", "collections/eventCollection"
                 //});
 
                 //
+            },
 
+            _populateDetailedView: function(event) {
+                console.log(event.get("number"));
+                $("#eventName").html(event.get("name"));
+                $("#peopleLeft").html(event.get("number"));
 
+            },
+            listView: function(){
+                console.log("ping from REAL list");
+                var eventCollection = new EventCollection();
+                eventCollection.fetch({
+                    success: function(events) {
+                        events.each(function(item){
+                            var template = $("#booktemplate").html();
+                            var compiled = Handlebars.compile(template);
+                            var html = compiled(item.attributes);
+                            console.log(html);
+                            $("#itemList").html(html);
+                        });
 
-
+                    }
+                });
             }
         });
         return Router;
