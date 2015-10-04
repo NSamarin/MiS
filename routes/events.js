729e13e62b4babb0f1302fc017856b8c88aaf4ef
comRamona <com.ramona@yahoo.com>
2015-10-04 15:32:07 +0100

    Mes

--- routes/events.js
+++ routes/events.js
@@ -62,6 +62,7 @@ pg.connect(DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
   client.query("CREATE TABLE IF NOT EXISTS events(name varchar(64), dates varchar(64),discount float, number int)");
+  client.query("INSERT INTO events(name,dates,discount,number) values($1,$2,$3,$4)",["a","oct",23,4]);
   var query = client.query("SELECT name,dates,discount,number from events");
    query.on('row', function(row) {
       results.push(row);
@@ -76,23 +77,23 @@ pg.connect(DATABASE_URL, function(err, client) {
 
 });
 router.post('/', function (req, res, next) {
-    var pg = require('pg');
+ /*   var pg = require('pg');
     console.log(req.body.name);
 DATABASE_URL='postgres://qxvgprniwlpgjm:HbD3NYtAQdFL7xk5eRjzxRZ7eD@ec2-54-83-51-38.compute-1.amazonaws.com:5432/da85113i577oud?ssl=true'
 
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-  client.query("CREATE TABLE IF NOT EXISTS evs(firstname varchar(64), lastname varchar(64))");
+  client.query("CREATE TABLE IF NOT EXISTS events(firstname varchar(64), lastname varchar(64))");
   var x=req.body.name;
-  client.query("INSERT INTO evs(firstname, lastname) values($1, $2)", [x, 'Ganesh']);
+  client.query("INSERT INTO events(firstname, lastname) values($1, $2)", [x, 'Ganesh']);
 
   var query = client.query("SELECT firstname, lastname FROM evs ORDER BY lastname, firstname");
    query.on('row', function(row) {
       console.log(JSON.stringify(row));
     });
 });
-    res.json(sample);
+    res.json(sample); */
 });
    
 
